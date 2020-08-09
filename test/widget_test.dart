fd84cb04b5634e7002747e5bf0661fb17edcbece
wasabeef <dadadada.chop@gmail.com>
2020-08-09 23:23:34 +0900

    add articleitem test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,23 +1,29 @@
-import 'package:app/app.dart';
+import 'package:app/ui/component/article_item.dart';
 import 'package:app/ui/component/loading.dart';
 import 'package:app/ui/detail/datail_page.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_test_utils/image_test_utils.dart';
 import 'package:mockito/mockito.dart';
 
+import 'data/dummy/dummy_article.dart';
+
 mixin MockDetailPage implements Mock, DetailPage {}
 
 void main() {
-  testWidgets('App', (WidgetTester tester) async {
+  testWidgets('Article widget test', (WidgetTester tester) async {
     await provideMockedNetworkImages(() async {
       await tester.pumpWidget(ProviderScope(
-//        overrides: [],
-        child: App(),
+        child: MaterialApp(
+          home: ArticleItem(
+            article: dummyArticle,
+          ),
+        ),
       ));
     });
   });
-  testWidgets('Loading', (WidgetTester tester) async {
+  testWidgets('Loading widget test', (WidgetTester tester) async {
     await tester.pumpWidget(const Loading());
   });
 }
