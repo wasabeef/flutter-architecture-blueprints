124c52a1bbbaffb4796cb5c2880ad020ed6a3931
wasabeef <dadadada.chop@gmail.com>
2020-08-09 20:03:42 +0900

    update sharedpreference mock

--- test/app_theme_test.dart
+++ test/app_theme_test.dart
@@ -1,39 +1,27 @@
 import 'package:app/data/model/theme_setting.dart';
 import 'package:app/data/provier/theme_data_source_provider.dart';
-import 'package:app/data/provier/theme_repository_provider.dart';
 import 'package:app/ui/app_theme.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mockito/mockito.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'data/local/fake_theme_data_source_impl.dart';
-import 'data/repository/fake_theme_repository_impl.dart';
 
 class MockAppTheme extends Mock implements AppTheme {}
 
 void main() {
+  setUpAll(() async {
+    SharedPreferences.setMockInitialValues(
+        <String, String>{'theme_setting': 'dark'});
+  });
+
   test('AppTheme + ThemeRepository Test', () async {
     final container = ProviderContainer(
       overrides: [
-        themeRepositoryProvider
-            .overrideWithValue(AsyncValue.data(FakeThemeRepositoryImpl())),
-      ],
-    );
-    final appTheme = container.read(appThemeNotifierProvider);
-    await expectLater(appTheme.themeData, completion(darkTheme));
-    expect(appTheme.setting, ThemeSetting.DARK);
-
-    await appTheme.toggle();
-
-    await expectLater(appTheme.themeData, completion(lightTheme));
-    expect(appTheme.setting, ThemeSetting.LIGHT);
-  });
-
-  test('AppTheme + DataSource Test', () async {
-    final container = ProviderContainer(
-      overrides: [
-        themeDataSourceProvider
-            .overrideWithValue(AsyncValue.data(FakeThemeDataSourceImpl())),
+        // or use SharedPreferences.setMockInitialValues
+        // themeDataSourceProvider
+        //    .overrideWithValue(AsyncValue.data(FakeThemeDataSourceImpl())),
       ],
     );
     final appTheme = container.read(appThemeNotifierProvider);
