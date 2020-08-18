44a1b5fb46b8a0fe174dd2b34ba59a2fc7ea6c28
wasabeef <dadadada.chop@gmail.com>
2020-08-19 00:31:37 +0900

    add comments for color codes

--- lib/ui/app_theme.dart
+++ lib/ui/app_theme.dart
@@ -7,6 +7,36 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+// Color converter: https://www.w3schools.com/colors/colors_converter.asp
+// Transparency list
+// 100% FF
+// 95%  F2
+// 90%  E6
+// 87%  DE
+// 85%  D9
+// 80%  CC
+// 75%  BF
+// 70%  B3
+// 65%  A6
+// 60%  99
+// 55%  8C
+// 54%  8A
+// 50%  80
+// 45%  73
+// 40%  66
+// 35%  59
+// 32%  52
+// 30%  4D
+// 26%  42
+// 25%  40
+// 20%  33
+// 16%  29
+// 15%  26
+// 12%  1F
+// 10%  1A
+// 5%   0D
+// 0%   00
+
 final appThemeNotifierProvider =
     ChangeNotifierProvider<AppTheme>((ref) => AppTheme(ref));
 
