5f50265408bee2942d8cbe7395ef7a97690df1f1
wasabeef <dadadada.chop@gmail.com>
2020-11-04 00:32:43 +0900

    test(data): fix test data

--- test/data/dummy/dummy_article.dart
+++ test/data/dummy/dummy_article.dart
@@ -8,8 +8,6 @@ final dummyArticle = Article(
   description:
       'Flutter Architecture Blueprint is a project that introduces MVVM architecture and project structure approaches to developing Flutter apps.',
   url: 'https://github.com/wasabeef/flutter-architecture-blueprints',
-  urlToImage:
-      'https://github.com/wasabeef/flutter-architecture-blueprints/raw/master/art/cover.png',
   publishedAt: DateTime.now(),
   content: 'content',
 );
