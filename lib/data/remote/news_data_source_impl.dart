5c39b731eac7c16de56f83f5c409b5e1c4ac9a6a
wasabeef <dadadada.chop@gmail.com>
2020-11-13 19:54:11 +0900

    fix(dio): rebert dio cache

--- lib/data/remote/news_data_source_impl.dart
+++ lib/data/remote/news_data_source_impl.dart
@@ -16,23 +16,21 @@ class NewsDataSourceImpl implements NewsDataSource {
 
   @override
   Future<News> getNews() async {
-    return _dio
-        .get<Map<String, dynamic>>(
-          '/v2/everything',
-          queryParameters: <String, String>{
-            'q': ['anim', 'manga'][Random().nextInt(2)], // For checking reload.
-            'from': DateTime.now()
-                .subtract(
-                  const Duration(days: 28),
-                )
-                .toLocal()
-                .formatYYYYMMdd(),
-            'sortBy': 'publishedAt',
-            'language': 'en',
-            'apiKey': Constants.of().apiKey,
-          },
-          // options: buildCacheOptions(const Duration(hours: 1)),
-        )
-        .then((response) => News.fromJson(response.data));
+    return _dio.get<Map<String, dynamic>>(
+      '/v2/everything',
+      queryParameters: <String, String>{
+        'q': ['anim', 'manga'][Random().nextInt(2)], // For checking reload.
+        'from': DateTime.now()
+            .subtract(
+              const Duration(days: 28),
+            )
+            .toLocal()
+            .formatYYYYMMdd(),
+        'sortBy': 'publishedAt',
+        'language': 'en',
+        'apiKey': Constants.of().apiKey,
+      },
+      options: buildCacheOptions(const Duration(hours: 1)),
+    ).then((response) => News.fromJson(response.data));
   }
 }
