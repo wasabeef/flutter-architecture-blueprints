72dd5ba1a109023436147bc27ec70e1adeb864eb
wasabeef <dadadada.chop@gmail.com>
2020-11-14 17:55:38 +0900

    fix(test): deleted meaningless tests

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -1,9 +1,9 @@
-import 'package:app/util/error_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import '../../util/error_snackbar.dart';
 import '../app_theme.dart';
 import '../component/article_item.dart';
 import '../component/loading.dart';
