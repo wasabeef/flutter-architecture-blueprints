fec0b4b21e180d0c30a91c79ab8736e2a152ce94
wasabeef <dadadada.chop@gmail.com>
2020-11-19 16:16:17 +0900

    refactor(error): add try-catch wrapper

--- lib/data/model/result.dart
+++ lib/data/model/result.dart
@@ -10,9 +10,25 @@ abstract class Result<T> with _$Result<T> {
   const factory Result.success({@required T data}) = Success<T>;
 
   const factory Result.failure({@required AppError error}) = Failure<T>;
+
+  factory Result.guard(T Function() body) {
+    try {
+      return Result.success(data: body());
+    } on Exception catch (e) {
+      return Result.failure(error: AppError(e));
+    }
+  }
+
+  static Future<Result<T>> guardFuture<T>(Future<T> Function() future) async {
+    try {
+      return Result.success(data: await future());
+    } on Exception catch (e) {
+      return Result.failure(error: AppError(e));
+    }
+  }
 }
 
-extension ResultExt on Result {
+extension ResultExt<T> on Result<T> {
   bool get isSuccess => when(success: (data) => true, failure: (e) => false);
 
   bool get isError => !isSuccess;
--- lib/data/repository/news_repository_impl.dart
+++ lib/data/repository/news_repository_impl.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../app_error.dart';
 import '../model/news.dart';
 import '../model/result.dart';
 import '../remote/news_data_source.dart';
@@ -14,10 +13,6 @@ class NewsRepositoryImpl implements NewsRepository {
 
   @override
   Future<Result<News>> getNews() async {
-    try {
-      return Result.success(data: await _dataSource.getNews());
-    } on Exception catch (e) {
-      return Result.failure(error: AppError(e));
-    }
+    return Result.guardFuture(_dataSource.getNews);
   }
 }
