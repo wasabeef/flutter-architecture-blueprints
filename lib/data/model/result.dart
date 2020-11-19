88447aa0738a603a03a4ada7831ade7ff6a02890
Daichi Furiya <dadadada.chop@gmail.com>
2020-11-19 18:31:44 +0900

    refactor(error): add try-catch wrapper (#43)
    * refactor(error): add try-catch wrapper
    
    * refactor(error): avoid unused generics
    
    * refactor(error): change to static method

--- lib/data/model/result.dart
+++ lib/data/model/result.dart
@@ -10,6 +10,22 @@ abstract class Result<T> with _$Result<T> {
   const factory Result.success({@required T data}) = Success<T>;
 
   const factory Result.failure({@required AppError error}) = Failure<T>;
+
+  static Result<T> guard<T>(T Function() body) {
+    try {
+      return Result.success(data: body());
+    } on Exception catch (e) {
+      return Result.failure(error: AppError(e));
+    }
+  }
+
+  static Future<Result<T>> guardFuture<T>(Future<T> Function() future) async {
+    try {
+      return Result.success(data: await future());
+    } on Exception catch (e) {
+      return Result.failure(error: AppError(e));
+    }
+  }
 }
 
 extension ResultExt on Result {
