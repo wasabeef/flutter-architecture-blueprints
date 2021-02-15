10153e9ca5d980a26aef34b34f9a68de9e466990
wasabeef <dadadada.chop@gmail.com>
2021-02-15 23:18:59 +0900

    chore: update import

--- lib/ui/loading_state_view_model.dart
+++ lib/ui/loading_state_view_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-import 'package:hooks_riverpod/all.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final loadingStateProvider =
     ChangeNotifierProvider((ref) => LoadingStateViewModel());
